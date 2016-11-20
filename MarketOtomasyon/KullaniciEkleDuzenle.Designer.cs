34b85b1677475d2fd23b1892a1c552bc45fae040
Fatih Baytar <fatih.baytar@gmail.com>
2016-11-20 19:59:25 +0300

    Yeni Kullanıcı Ekleme kısmı yapıldı.

--- MarketOtomasyon/KullaniciEkleDuzenle.Designer.cs
+++ MarketOtomasyon/KullaniciEkleDuzenle.Designer.cs
@@ -36,16 +36,19 @@
             this.chkAdmin = new System.Windows.Forms.CheckBox();
             this.txbKullaniciAdi = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.txbSifre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnKaydet
             // 
-            this.btnKaydet.Location = new System.Drawing.Point(76, 198);
+            this.btnKaydet.Location = new System.Drawing.Point(91, 230);
             this.btnKaydet.Name = "btnKaydet";
             this.btnKaydet.Size = new System.Drawing.Size(138, 44);
-            this.btnKaydet.TabIndex = 5;
+            this.btnKaydet.TabIndex = 6;
             this.btnKaydet.Text = "Kaydet";
             this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
             // 
             // txbAd
             // 
@@ -57,7 +60,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(88, 28);
+            this.label1.Location = new System.Drawing.Point(88, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(25, 18);
             this.label1.TabIndex = 2;
@@ -73,7 +76,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(63, 70);
+            this.label2.Location = new System.Drawing.Point(63, 71);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 18);
             this.label2.TabIndex = 2;
@@ -82,10 +85,10 @@
             // chkAdmin
             // 
             this.chkAdmin.AutoSize = true;
-            this.chkAdmin.Location = new System.Drawing.Point(120, 151);
+            this.chkAdmin.Location = new System.Drawing.Point(119, 202);
             this.chkAdmin.Name = "chkAdmin";
             this.chkAdmin.Size = new System.Drawing.Size(68, 22);
-            this.chkAdmin.TabIndex = 4;
+            this.chkAdmin.TabIndex = 5;
             this.chkAdmin.Text = "Admin";
             this.chkAdmin.UseVisualStyleBackColor = true;
             // 
@@ -99,11 +102,27 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 112);
+            this.label3.Location = new System.Drawing.Point(27, 113);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(86, 18);
             this.label3.TabIndex = 2;
             this.label3.Text = "Kullanıcı Adı";
+            // 
+            // txbSifre
+            // 
+            this.txbSifre.Location = new System.Drawing.Point(119, 155);
+            this.txbSifre.Name = "txbSifre";
+            this.txbSifre.Size = new System.Drawing.Size(131, 24);
+            this.txbSifre.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 18);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Şifre";
             // 
             // KullaniciEkleDuzenle
             // 
@@ -111,9 +130,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(314, 318);
             this.Controls.Add(this.chkAdmin);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbSifre);
             this.Controls.Add(this.txbKullaniciAdi);
             this.Controls.Add(this.txbSoyad);
             this.Controls.Add(this.txbAd);
@@ -122,6 +143,7 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "KullaniciEkleDuzenle";
             this.Text = "KullaniciEkleDuzenle";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.KullaniciEkleDuzenle_FormClosed);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,5 +159,7 @@
         private System.Windows.Forms.CheckBox chkAdmin;
         private System.Windows.Forms.TextBox txbKullaniciAdi;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbSifre;
+        private System.Windows.Forms.Label label4;
     }
 }
--- MarketOtomasyon/KullaniciEkleDuzenle.cs
+++ MarketOtomasyon/KullaniciEkleDuzenle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarketOtomasyon.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,49 @@ namespace MarketOtomasyon
         public KullaniciEkleDuzenle()
         {
             InitializeComponent();
+        }
+
+        private void KullaniciEkleDuzenle_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            foreach (Form siradakiForm in Application.OpenForms)
+            {
+                if (siradakiForm.Name=="AdminForm")
+                {
+                    siradakiForm.Show();
+                }
+            }
+        }
+        MarketDBEntities ent = new MarketDBEntities();
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            Kullanici yeniKullanici = new Kullanici();
+
+            if (ent.Kullanici.FirstOrDefault(k=>k.KullaniciAdi==txbKullaniciAdi.Text)!=null)
+            {
+                MessageBox.Show("Bu kullanıcı adını kullanamazsınız");
+                return;
+            }
+            if (string.IsNullOrEmpty(txbAd.Text)||string.IsNullOrEmpty(txbSoyad.Text)||string.IsNullOrEmpty(txbKullaniciAdi.Text)||string.IsNullOrEmpty(txbSifre.Text))
+            {
+                MessageBox.Show("Bu alanlar boş geçilemez");
+                return;
+            }
+            yeniKullanici.Ad = txbAd.Text;
+            yeniKullanici.Soyad = txbSoyad.Text;
+            yeniKullanici.KullaniciAdi = txbKullaniciAdi.Text;
+            yeniKullanici.Sifre = txbSifre.Text;
+            yeniKullanici.Yetki = chkAdmin.Checked ?Convert.ToInt16(1) :Convert.ToInt16(0);
+            ent.Kullanici.Add(yeniKullanici);
+            MessageBox.Show(ent.SaveChanges()>0?"Yeni Kullanıcı Başarı ile eklendi":"Bir sorun oluştu");
+
+            foreach (Form siradakiForm in Application.OpenForms)
+            {
+                if (siradakiForm.Name=="AdminForm")
+                {
+                    (siradakiForm as AdminForm).ListBoxDoldur("");
+                }
+            }
+
         }
     }
 }
